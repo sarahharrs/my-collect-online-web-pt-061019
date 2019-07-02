9b6589e4a1c0ce5b0d5ff8c982e201d61a4588c0
Sarah Harrs <sarahharrs@gmail.com>
2019-07-02 00:16:17 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,10 @@
 
-
+def my_collect (collection)
+i= 0
+collection = ['ruby', 'javascript', 'python', 'objective-c']
+while i < collection.length
+collection << yield (collection [i])
+i += 1
+end
+collection
+end
