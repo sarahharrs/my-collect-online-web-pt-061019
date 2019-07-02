5ec30e9ca8905dac5412048da3f7e016a3d8a2e0
Sarah Harrs <sarahharrs@gmail.com>
2019-07-02 00:18:49 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,10 +1,9 @@
-
-def my_collect (array)
-i= 0
-collection = []
-while i < array.length
-collection << yield (array [i])
-i += 1
-end
-collection
+def my_collect(array)
+  i = 0
+  collect = []
+  while i < array.length
+    collect << yield(array[i])
+    i+=1
+  end
+  collect
 end
